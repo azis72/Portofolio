737e71091455a8e56bac9b91bbdebee42521e1c7
azis72 <139344708+azis72@users.noreply.github.com>
2023-11-24 21:15:08 +0700

    create 1st Js/main.js

---
+++ 1st Js/main.js
@@ -0,0 +1,30 @@
+const selectL = document.getElementById('addItem');
+      table = document.querySelector("table");
+selectL.addEventListener("click", function() {
+  let newName = prompt("Masukkan Nama!");
+      newAge = parseInt(prompt("Masukkan Usia!"));
+  if ( newName == "" || newAge == "" || typeof(newAge) != "number") {
+    alert("Yg bener lah goblok!!");
+  } else if ( !newName || !newAge || newName == " ") {
+  alert("Hadahh.. taikk lu!");
+  } else {
+    var newTr = document.createElement("tr");
+        newTd1 = document.createElement("td");
+        newTd2 = document.createElement("td");
+        newListName = document.createTextNode(newName);
+        newListAge = document.createTextNode(newAge);
+      newTd1.appendChild(newListName);
+      newTd2.appendChild(newListAge);
+      newTr.appendChild(newTd1);
+      newTr.appendChild(newTd2);
+      table.appendChild(newTr);
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+});
